787067a9e59940cd7b638d0beb6818d36fb3ed35
Zekun Li <zli203@uottawa.ca>
2019-06-02 19:24:08 -0400

    Update menu json

---
+++ menu.js
@@ -0,0 +1,9 @@
+//item id to menu
+//import <script src='menu.js' type = 'text/javascript'></script> at head
+//use menu["1"] to get string "Egg McMuffin Extra Value Meal"
+
+
+var menu = {
+    "1": "Egg McMuffin Extra Value Meal",
+    "2": "Sausage 'N Egg McMuffin Extra Value Meal"
+}
