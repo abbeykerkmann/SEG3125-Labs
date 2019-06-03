a752613b6acbaccceaeba5be5d39cd7ad6d85c72
abbeykerkmann <abbey.kerkmann@gmail.com>
2019-06-03 17:14:03 -0400

    added menu items

--- menu.js
+++ menu.js
@@ -5,5 +5,14 @@
 
 var menu = {
     "1": "Egg McMuffin Extra Value Meal",
-    "2": "Sausage 'N Egg McMuffin Extra Value Meal"
+    "2": "Sausage 'N Egg McMuffin Extra Value Meal",
+    "3": "Bacon 'N Egg McMuffin Extra Value Meal",
+    "4": "Sausage & Hash Brown McWrap Extra Value Meal",
+    "5": "Bacon & Hash Brown McWrap Extra Value Meal",
+    "6": "2 Breakfast Burritos Extra Value Meal",
+    "7": "Sausage, Egg & Cheese McGriddles Extra Value Meal",
+    "8": "Bacon, Egg & Cheese McGriddles Extra Value Meal",
+    "9": "Sausage McGriddle Extra Value Meal",
+    "10": "Big Breakfast",
+    "11": "Happy Meal Hotcakes"
 }
