81ba3c33cb800ba61ef00b4b17c5368c88876e3a
Zekun Li <zli203@uottawa.ca>
2019-06-03 22:07:40 -0400

    update order

--- menu.js
+++ menu.js
@@ -4,6 +4,15 @@
 
 
 var menu = {
-    "1": "Egg McMuffin Extra Value Meal",
-    "2": "Sausage 'N Egg McMuffin Extra Value Meal"
+    "1": { "name": "Egg McMuffin Extra Value Meal", "price": 6.39 },
+    "2": { "name": "Sausage 'N Egg McMuffin Extra Value Meal", "price": 6.49 },
+    "3": { "name": "Bacon 'N Egg McMuffin Extra Value Meal", "price": 6.49 },
+    "4": { "name": "Sausage & Hash Brown McWrap Extra Value Meal", "price": 6.59 },
+    "5": { "name": "Bacon & Hash Brown McWrap Extra Value Meal", "price": 6.59 },
+    "6": { "name": "2 Breakfast Burritos Extra Value Meal", "price": 7.39 },
+    "7": { "name": "Sausage, Egg & Cheese McGriddles Extra Value Meal", "price": 6.39 },
+    "8": { "name": "Bacon, Egg & Cheese McGriddles Extra Value Meal", "price": 6.39 },
+    "9": { "name": "Sausage McGriddle Extra Value Meal", "price": 6.19 },
+    "10": { "name": "Big Breakfast", "price": 4.69 },
+    "11": { "name": "Happy Meal Hotcakes", "price": 4.19 }
 }
